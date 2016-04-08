1d412e8b49e33da09a49deb50e6001b6dd3aae78
Michal Sawicki <michal@mobilewebpro.pl>
2016-04-08 12:32:14 +0200

    another js methods tested on playground

--- src/views/PublishingApp.js
+++ src/views/PublishingApp.js
@@ -72,15 +72,45 @@ class PublishingApp extends React.Component {
     let mockedString = "los angeles california United States";
     let anotherString = "beatiful city with lovely views";
     let usaCities = ["waszyngton", "dallas", "denver", "honolulu"];
-    console.log(usaCities.join());
-    this.props.articleActions.joinArray(usaCities.join(" "));
+    console.log(usaCities.join(" "));
+    let joinedCities = usaCities.join("\n");
+    let inputValuesFromReducer = this.props.formDataReducer;
+    console.info(inputValuesFromReducer);
+    this.props.articleActions.joinArray(joinedCities);
+
+
+
+    function toTitleCase(str)
+      {
+        return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+      }
   }
 
   render () {
     console.info("from reducer -> ", this.props.articleReducer);
     let personNamesFromReducer = this.props.articleReducer;
+    let citiesArray = String(this.props.articleReducer);
+    let citiesArrayToUppercase = citiesArray.split(' ').map( character => character.charAt(0)
+      .toUpperCase() + character.slice(1))
+      .join(' ');
+      console.info(citiesArrayToUppercase);
 
-    let mapPersonsJSX = [];
+    return (
+      <div>
+          <h1 className="row center-lg center-md center-sm center-xs">Our publishing app - Javascript Playground</h1>
+          
+          <h2 onClick={this._tryArrayJoin} className="row center-lg center-md center-sm center-xs">Array Join Show</h2>
+          {citiesArray}
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PublishingApp);
+
+
+
+ /* let mapPersonsJSX = [];
     for(let personKey in personNamesFromReducer) {
       let personDetails = personNamesFromReducer[personKey];
       let currentArticleJSX = (
@@ -88,34 +118,44 @@ class PublishingApp extends React.Component {
           <h2>{personDetails}</h2>
         </div>);
       mapPersonsJSX.push(currentArticleJSX)
-      };
-
-      let citiesArray = this.props.articleReducer;
-      let citiesArrayConverted = JSON.stringify(citiesArray);
-      console.log(citiesArrayConverted);
-   
-  	/*let articlesJSX = [];
-  	for(let articleKey in this.props.articleReducer) {
-  		let articleDetails = this.props.articleReducer[articleKey];
-  		let currentArticleJSX = (
-  			<div key={articleKey}>
-  				<h2>{articleDetails.articleTitle}</h2>
-  				<h3>{articleDetails.articleContent}</h3>
-
-
-  			</div>);
-  		articlesJSX.push(currentArticleJSX)
       };*/
-  	
-    return (
-      <div>
-          <h1 className="row center-lg center-md center-sm center-xs">Our publishing app - Javascript Playground</h1>
-          
-          <h2 onClick={this._tryArrayJoin} className="row center-lg center-md center-sm center-xs">Array Join Show</h2>
-          {citiesArrayConverted}
-      </div>
-    );
-  }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(PublishingApp);
+     
+      /*let citiesArrayConverted = JSON.stringify(citiesArray);*/
+
+   
+    /*let articlesJSX = [];
+    for(let articleKey in this.props.articleReducer) {
+      let articleDetails = this.props.articleReducer[articleKey];
+      let currentArticleJSX = (
+        <div key={articleKey}>
+          <h2>{articleDetails.articleTitle}</h2>
+          <h3>{articleDetails.articleContent}</h3>
+
+
+        </div>);
+      articlesJSX.push(currentArticleJSX)
+      };*/
+
+
+     /* let newNyc = [];
+      let newPropNyc =[];
+
+
+      function myFunction() {
+
+        let nyc = {
+            fullName: "New York City",
+            mayor: "Michael Bloomberg",
+            population: 8000000,
+            boroughs: 5
+        };
+        let propValue;
+        for(let propName in nyc) {
+            propValue = nyc[propName]
+
+            newNyc.push(propValue);
+            newPropNyc.push(propName);
+        }
+      };
+      myFunction();*/
