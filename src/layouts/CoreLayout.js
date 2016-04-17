2ab6b81ec529b99d5b2a2c9a38d14cd5f854dbf2
Michal Sawicki <michal@mobilewebpro.pl>
2016-04-17 11:51:32 +0200

    core layout modified

--- src/layouts/CoreLayout.js
+++ src/layouts/CoreLayout.js
@@ -17,6 +17,9 @@ const mapDispatchToProps = (dispatch) => ({
   // sessionActions: bindActionCreators(sessionActions, dispatch)
 });
 
+
+
+
 class CoreLayout extends React.Component {
   constructor(props) {
     super(props);
@@ -26,7 +29,9 @@ class CoreLayout extends React.Component {
 
     this.handleToggle = this.handleToggle.bind(this);
   }
-  handleToggle = () => this.setState({open: !this.state.open});
+  handleToggle = () => this.setState({
+    open: !this.state.open 
+  });
 
   render () {
 
@@ -54,7 +59,7 @@ class CoreLayout extends React.Component {
               icon={<Accessability />} />
         </Link>
         </LeftNav>
-        <div>
+        <div >
           {this.props.children}
         </div>
       </div>
@@ -64,71 +69,4 @@ class CoreLayout extends React.Component {
 
 
 export default connect(mapStateToProps, mapDispatchToProps)(CoreLayout);
-
-
-/*import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import API from 'utils/API';
-import { Link } from 'react-router';
-import { Paper, FlatButton, AppBar } from 'material-ui';
-import LeftNav from 'material-ui/lib/left-nav';
-import RaisedButton from 'material-ui/lib/raised-button';
-import Reorder from 'material-ui/lib/svg-icons/action/reorder';
-import Accessability from 'material-ui/lib/svg-icons/action/account-box';
-
-const mapStateToProps = (state) => ({
-  ...state
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  // sessionActions: bindActionCreators(sessionActions, dispatch)
-});
-
-class CoreLayout extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {open: false};
-
-    this.handleToggle = this.handleToggle.bind(this);
-  }
-  handleToggle = () => this.setState({open: !this.state.open});
-
-  render () {
-
-    return (
-      <div >
-        <Reorder
-          style={{width:50, height:50}}
-          onTouchTap={this.handleToggle}
-        />
-        <LeftNav width={150} openRight={false} open={this.state.open} >
-          <Reorder
-          style={{width:50, height:50}}
-          onTouchTap={this.handleToggle}
-        />
-        <Link to='/'>
-            <FlatButton
-              backgroundColor="#ffffff"
-              label="Home"
-              icon={<Accessability />} />
-        </Link>
-        <Link to='/form'>
-            <FlatButton
-              backgroundColor="#ffffff"
-              label="Form"
-              icon={<Accessability />} />
-        </Link>
-        </LeftNav>
-        <div>
-          {this.props.children}
-        </div>
-      </div>
-    );
-  }
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(CoreLayout);*/
-
 
