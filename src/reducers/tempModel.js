6c7f04b70bdaba8186359c677d62578736549bb9
Michal Sawicki <michal@mobilewebpro.pl>
2016-04-21 11:35:21 +0200

    tempModel clients view objects done

--- src/reducers/tempModel.js
+++ src/reducers/tempModel.js
@@ -9,6 +9,7 @@ at the start data need to be populated , before there will be possibility to ent
     { 
       id: "1", 
       name: "first client",
+      faceAmount: 50000,
       firstClientChildrens: [
         {
           age: 8,
@@ -25,32 +26,71 @@ at the start data need to be populated , before there will be possibility to ent
       ],
       quotesId: ["firstClient123456"],
       underwritingOptions: 
-  [
-  	{
-		underwritingId: 1,
-		underwritingName:"Super Preferred"
-	},
-	{
-		underwritingId: 2,
-		underwritingName: "Preferred"
-	},
-	{
-		underwritingId: 3,
-		underwritingName: "Standard Plus"
-	},
-	{
-		underwritingId: 4,
-		underwritingName: "Standard"
-	}
-  ],
-  clientAgeData: { 
+	  [
+	  	{
+			underwritingId: 1,
+			underwritingName:"Super Preferred"
+		},
+		{
+			underwritingId: 2,
+			underwritingName: "Preferred"
+		},
+		{
+			underwritingId: 3,
+			underwritingName: "Standard Plus"
+		},
+		{
+			underwritingId: 4,
+			underwritingName: "Standard"
+		}
+	  ],
+  	clientAgeData: { 
 	  	actualAge: 30, 
 	  	nearestAge: 31
-  	}
+  		},
+  	policyOptions: 
+  	[
+	  	{
+			policyId: 1,
+			policyName:"Term 10"
+		},
+		{
+			policyId: 2,
+			policyName: "Term 20"
+		},
+		{
+			policyId: 3,
+			policyName: "Term 30"
+		},
+		{
+			policyId: 4,
+			policyName: "Term 40"
+		}
+	],
+	provinceList: 
+  	[
+	  	{
+			provinceId: 1,
+			provinceName:"Ontario"
+		},
+		{
+			provinceId: 2,
+			provinceName: "Quebec"
+		},
+		{
+			provinceId: 3,
+			provinceName: "New Scotland"
+		},
+		{
+			provinceId: 4,
+			provinceName: "Alberta"
+		}
+	]
     },
     { 
       id: "2",
      name: "second client",
+     faceAmount: 50000,
      secondClientChildrens: [
         {
           age: 11,
@@ -80,57 +120,27 @@ at the start data need to be populated , before there will be possibility to ent
   clientAgeData: { 
 	  	actualAge: 50, 
 	  	nearestAge: 51
-  	}
-      }
-  ],
-  licenseInfo: {
-    "key": "unique license key",
-    "clientName": "my client LTD",
-    "description": "licensed to someone , someone under some conditions"
-  },
-  quotesResults: {
-    "tableQouteId": {
-      quoteTableData: [
-        {
-          companyName: "first insurance company",
-          companyProduct: "first insurance company product",
-          productPrice: "15000"
-        },
-        {
-          companyName: "second insurance company",
-          companyProduct: "second insurance company product",
-          productPrice: "35000"
-        },
-        {
-          companyName: "third insurance company",
-          companyProduct: "third insurance company product",
-          productPrice: "75000"
-        }
-      ]
-    }
-  },
-  
-  policyOptions: 
-  [
-  	{
-		policyId: 1,
-		policyName:"Term 10"
-	},
-	{
-		policyId: 2,
-		policyName: "Term 20"
-	},
-	{
-		policyId: 3,
-		policyName: "Term 30"
-	},
-	{
-		policyId: 4,
-		policyName: "Term 40"
-	}
-  ],
-  
-  	provinceList: 
+  	},
+  	policyOptions: 
+  	[
+	  	{
+			policyId: 1,
+			policyName:"Term 10"
+		},
+		{
+			policyId: 2,
+			policyName: "Term 20"
+		},
+		{
+			policyId: 3,
+			policyName: "Term 30"
+		},
+		{
+			policyId: 4,
+			policyName: "Term 40"
+		}
+	],
+	provinceList: 
   	[
 	  	{
 			provinceId: 1,
@@ -149,5 +159,36 @@ at the start data need to be populated , before there will be possibility to ent
 			provinceName: "Alberta"
 		}
 	]
+      }
+  ],
+
+  licenseInfo: {
+    "key": "unique license key",
+    "clientName": "my client LTD",
+    "description": "licensed to someone , someone under some conditions"
+  },
+  quotesResults: {
+    "tableQouteId": {
+      quoteTableData: [
+        {
+          companyName: "FaithLife Financial",
+          companyProduct: "FaithLife Financial product",
+          productPrice: "15000"
+        },
+        {
+          companyName: "Sun Life",
+          companyProduct: "Sun Life product",
+          productPrice: "35000"
+        },
+        {
+          companyName: "Manufacturers Life Insurance Company",
+          companyProduct: "Manufacturers Life Insurance Company product",
+          productPrice: "75000"
+        }
+      ]
+    }
+  },
+
+	
 
 }
